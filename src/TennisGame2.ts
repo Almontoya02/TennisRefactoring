f75f80a1b20f6478e4ebc8310f27d0fa6c9b2ab7
Alejandro Montoya <almontoya02@outlook.com>
2022-05-17 20:24:41 -0500

    2

--- src/TennisGame2.ts
+++ src/TennisGame2.ts
@@ -5,6 +5,7 @@ export class TennisGame2 implements TennisGame {
 
   player1:Player;
   player2:Player;
+  
   constructor(player1:Player, player2:Player) {
     this.player1 = player1;
     this.player2 = player2;
